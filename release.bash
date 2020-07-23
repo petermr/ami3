ca064b0839deb00e8c166dbd5a5d0c93bf1eaf21
Remko Popma <remkop@yahoo.com>
2020-07-23 13:20:32 +0900

    [#51] fix release.bash: use `git commit` instead of `mvn scm:checkin` to limit the committed file to just pom.xml

--- release.bash
+++ release.bash
@@ -10,23 +10,29 @@ echo "Releasing v${RELEASE_VERSION}"
 
 sed -i'' "s/<version>[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]_[0-9][0-9].*</<version>${RELEASE_VERSION}</g" pom.xml
 
-#git commit -m "Bump ami version to ${VERSION}" pom.xml
+# The maven scm:checkin goal commits ALL modified files, not just pom.xml...
+# see https://stackoverflow.com/questions/48947392/commit-multiple-files-with-maven-scm-plugin
 # see https://maven.apache.org/scm/maven-scm-plugin/checkin-mojo.html
-mvn scm:checkin -Dmessage="Bump ami version to ${RELEASE_VERSION}" -Dincludes=pom.xml -DpushChanges=false
+# WAS: if mvn scm:checkin -Dmessage="Bump ami version to ${RELEASE_VERSION}" -Dincludes=pom.xml -DpushChanges=false
+if git commit -m "Bump ami version to ${RELEASE_VERSION}" --verbose pom.xml
+then
 
-#git tag -s -m "Release ami version ${VERSION}"
-# Use scm:check-local-modification to ensure all changes are committed before we tag the commit.
-# see https://maven.apache.org/scm/maven-scm-plugin/tag-mojo.html
-# and https://maven.apache.org/scm/maven-scm-plugin/check-local-modification-mojo.html
-mvn scm:check-local-modification scm:tag -Dmessage="Bump ami version to ${RELEASE_VERSION}" -DpushChanges=false -Dsign=true
+    #git tag -s -m "Release ami version ${VERSION}"
+    # Use scm:check-local-modification to ensure all changes are committed before we tag the commit.
+    # see https://maven.apache.org/scm/maven-scm-plugin/tag-mojo.html
+    # and https://maven.apache.org/scm/maven-scm-plugin/check-local-modification-mojo.html
+    if mvn scm:check-local-modification scm:tag -Dmessage="Bump ami version to ${RELEASE_VERSION}" -DpushChanges=false -Dsign=true
+    then
+        # mvn deploy to upload our distribution artifacts to GitHub Packages
+        # see https://docs.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-apache-maven-for-use-with-github-packages
+        #mvn -Darguments=-DskipTests -DskipTests -Dmaven.test.skip=true clean package  deploy
 
-# mvn deploy to upload our distribution artifacts to GitHub Packages
-# see https://docs.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-apache-maven-for-use-with-github-packages
-#mvn -Darguments=-DskipTests -DskipTests -Dmaven.test.skip=true clean package  deploy
-
-echo "Updating to v${DEVELOPMENT_VERSION} for next development cycle..."
-sed -i'' "s/<version>${RELEASE_VERSION}</<version>${DEVELOPMENT_VERSION}</g" pom.xml
-mvn scm:checkin -Dmessage="Updating ami version to ${DEVELOPMENT_VERSION} for next development cycle" -Dincludes=pom.xml -DpushChanges=true
-
+        echo "Updating to v${DEVELOPMENT_VERSION} for next development cycle..."
+        sed -i'' "s/<version>${RELEASE_VERSION}</<version>${DEVELOPMENT_VERSION}</g" pom.xml
+        # mvn scm:checkin -Dmessage="Updating ami version to ${DEVELOPMENT_VERSION} for next development cycle" -Dincludes=pom.xml -DpushChanges=true
+        git commit -m "Updating ami version to ${DEVELOPMENT_VERSION} for next development cycle" --verbose pom.xml
+        git push
+    fi
+fi
 echo "Done."
 
