5fda378780c65c32b0046978d52a1a8c61d8b819
Remko Popma <remkop@yahoo.com>
2020-07-23 12:41:02 +0900

    DOC removed offline copy of https://stackify.com/streams-guide-java-8/ article

--- doc/A Guide to Streams in Java 8  In-Depth Tutorial with Examples_files/eael-post-15652.js
+++
@@ -1,1 +0,0 @@
-!function(n){window.isEditMode=!1,n(window).on("elementor/frontend/init",function(){window.isEditMode=elementorFrontend.isEditMode()})}(jQuery);
--- doc/A Guide to Streams in Java 8  In-Depth Tutorial with Examples_files/logconsent.js
+++
@@ -1,5 +0,0 @@
-CookieConsent.setCookie("{stamp:'39vg+Z7ROlRBahaO6OCppTFUBuobVyZzeHS/o2QNYQfp6Lcn891sRw=='%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cver:1%2Cutc:1583102224600}", new Date(1614638224600), '/', '', '');
-if (typeof CookieConsent.resetBulkDomains!='undefined') {CookieConsent.resetBulkDomains(["gtm.stackify.com","sandbox.stackify.com","stackify.com"]);}
-CookieConsent.init();
-if (typeof CookieConsent.resetCookies!='undefined') {CookieConsent.resetCookies();}
-if (typeof CookieConsent.registerBulkConsent!='undefined') {CookieConsent.registerBulkConsent();}
