19e6047df60fd0e1e8fc60f73a8743bcaf822d04
Remko Popma <remkop@yahoo.com>
2020-07-23 14:56:23 +0900

    [#51] push the release tag when done

--- release.bash
+++ release.bash
@@ -31,7 +31,7 @@ then
         sed -i'' "s/<version>${RELEASE_VERSION}</<version>${DEVELOPMENT_VERSION}</g" pom.xml
         # mvn scm:checkin -Dmessage="Updating ami version to ${DEVELOPMENT_VERSION} for next development cycle" -Dincludes=pom.xml -DpushChanges=true
         git commit -m "Updating ami version to ${DEVELOPMENT_VERSION} for next development cycle" --verbose pom.xml
-        git push
+        git push --tags
     fi
 fi
 echo "Done."
